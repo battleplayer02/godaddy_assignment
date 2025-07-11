dca18326bb3e3720a7728c756afa9cd92f7e8a89
bfrs01757 <himanshu.shekhar@shiprocket.cim>
2025-07-11 17:50:23 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/godaddy_assignment/dist/',
   test: {
     globals: true,
     environment: 'jsdom',
